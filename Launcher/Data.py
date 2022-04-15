e13a61b5ea241d89b5b67bff31cc1cdea09a53f5
LemonadeCow <ferandocarrion@gmail.com>
2022-04-15 02:36:26 -0400

    [+] Support for epic_id , Folder class

--- Launcher/Data.py
+++ Launcher/Data.py
@@ -8,6 +8,7 @@ class Game:
         self.icon = ""
         self.exec = ""
         self.id = -1
+        self.epic_id = ""
     
     def to_json(self):
         tmp = {
@@ -15,6 +16,19 @@ class Game:
             "shortcut" : self.shortcut,
             "icon" : self.icon,
             "exec" : self.exec,
+            "epic_id" : self.epic_id,
             "id" : self.id
+        }
+        return tmp
+
+class Folder:
+    def __init__(self):
+        self.name = ""
+        self.games = []
+
+    def to_json(self):
+        tmp = {
+            "name" : self.name,
+            "games" : self.games
         }
         return tmp
